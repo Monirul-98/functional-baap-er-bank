52105ab90b34336537396a2da410252c60309620
Monirul-98 <monirulsifat@gmail.com>
2022-02-11 19:24:38 +0600

    Finish

--- js/banking.js
+++ js/banking.js
@@ -11,11 +11,16 @@ function addingTotal(totalInputId, amount) {
   const previousTotalInput = parseFloat(previousTotalInputText);
   totalInput.innerText = amount + previousTotalInput;
 }
-
-function updateBalance(amount, isAdd) {
+function balance() {
   const balanceTotal = document.getElementById("balance-total");
   const previousBalanceText = balanceTotal.innerText;
   const previousBalance = parseFloat(previousBalanceText);
+  return previousBalance;
+}
+
+function updateBalance(amount, isAdd) {
+  const balanceTotal = document.getElementById("balance-total");
+  const previousBalance = balance();
   if (isAdd == true) {
     balanceTotal.innerText = previousBalance + amount;
   } else {
@@ -25,12 +30,20 @@ function updateBalance(amount, isAdd) {
 
 document.getElementById("diposit-btn").addEventListener("click", function () {
   const newDepositAmount = getInput("deposit-input");
-  addingTotal("deposit-total", newDepositAmount);
-  updateBalance(newDepositAmount, true);
+  if (newDepositAmount > 0) {
+    addingTotal("deposit-total", newDepositAmount);
+    updateBalance(newDepositAmount, true);
+  }
 });
 
 document.getElementById("withdraw-btn").addEventListener("click", function () {
   const newWithdraw = getInput("withdraw-input");
-  addingTotal("withdraw-total", newWithdraw);
-  updateBalance(newWithdraw, false);
+  const balanceTotal = balance();
+  if (newWithdraw > 0 && newWithdraw < balanceTotal) {
+    addingTotal("withdraw-total", newWithdraw);
+    updateBalance(newWithdraw, false);
+  }
+  if (newWithdraw > balanceTotal) {
+    alert("Please withdraw amount what you have on your account!");
+  }
 });
