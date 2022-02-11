629ee2b6754220bb23d2d051df9758837724d115
Monirul-98 <monirulsifat@gmail.com>
2022-02-11 18:34:35 +0600

    Balance funtion added

--- js/banking.js
+++ js/banking.js
@@ -12,26 +12,25 @@ function addingTotal(totalInputId, amount) {
   totalInput.innerText = amount + previousTotalInput;
 }
 
-document.getElementById("diposit-btn").addEventListener("click", function () {
-  const newDepositAmount = getInput("deposit-input");
-
-  addingTotal("deposit-total", newDepositAmount);
-
+function updateBalance(amount, isAdd) {
   const balanceTotal = document.getElementById("balance-total");
   const previousBalanceText = balanceTotal.innerText;
   const previousBalance = parseFloat(previousBalanceText);
-  const newBalance = previousBalance + newDepositAmount;
-  balanceTotal.innerText = newBalance;
+  if (isAdd == true) {
+    balanceTotal.innerText = previousBalance + amount;
+  } else {
+    balanceTotal.innerText = previousBalance - amount;
+  }
+}
+
+document.getElementById("diposit-btn").addEventListener("click", function () {
+  const newDepositAmount = getInput("deposit-input");
+  addingTotal("deposit-total", newDepositAmount);
+  updateBalance(newDepositAmount, true);
 });
 
 document.getElementById("withdraw-btn").addEventListener("click", function () {
   const newWithdraw = getInput("withdraw-input");
-
   addingTotal("withdraw-total", newWithdraw);
-
-  const totalBalance = document.getElementById("balance-total");
-  const preBalanceText = totalBalance.innerText;
-  const preBalance = parseFloat(preBalanceText);
-  const balanceNew = preBalance - newWithdraw;
-  totalBalance.innerText = balanceNew;
+  updateBalance(newWithdraw, false);
 });
